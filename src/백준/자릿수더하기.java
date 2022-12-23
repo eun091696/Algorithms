75f2989855ab11f15049de1750bc7d761c6774df
Changeun An <eun091696@gmail.com>
2022-12-23 23:46:21 +0900

    12.23 프로그래머스Lv.1

---
+++ src/백준/자릿수더하기.java
@@ -0,0 +1,20 @@
+package 백준;
+
+public class 자릿수더하기 {
+	public int soultion(int n) {
+		int answer = 0;
+		String a = Integer.toString(n);
+		for(int i = 0; i < a.length(); i++) {
+			answer += Integer.parseInt(a.substring(i, i + 1));
+		}
+		
+		return answer;
+	
+}
+	public static void main(String[] args) {
+		자릿수더하기 g = new 자릿수더하기();
+		System.out.println(g.soultion(25));
+
+	}
+
+}
