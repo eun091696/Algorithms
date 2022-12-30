72be9c160facc0838d3212044ebb40a6640bc3aa
Changeun An <eun091696@gmail.com>
2022-12-30 17:54:27 +0900

    12.30 프로그래머스Lv.2 행렬의 곱셈

---
+++ src/프로그래머스/행렬의곱셈.java
@@ -0,0 +1,22 @@
+package 프로그래머스;
+
+public class 행렬의곱셈 {
+	public int[][] solution(int[][] arr1, int[][] arr2) {
+		int[][] answer = new int[arr1.length][arr2[0].length];
+		for(int i = 0; i < arr1.length; i++) {
+			for(int j = 0; j < arr2[0].length; j++) {
+				for(int k = 0; k < arr2.length; k++) {
+					answer[i][j] += arr1[i][k] * arr2[k][j];
+				}				
+			}
+		}
+		return answer;
+	}
+	public static void main(String[] args) {
+		행렬의곱셈 g = new 행렬의곱셈();
+		int[][] a = {{1,4}, {3,2}, {4,1}};
+		int[][] b = {{3,3}, {3,3}};
+		System.out.println("행렬의 곱셈: " +g.solution(a, b));
+	}
+
+}
