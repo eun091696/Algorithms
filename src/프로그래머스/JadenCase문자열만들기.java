5f1373659b8b41900ee47ce04391b429c28eceaf
Changeun An <eun091696@gmail.com>
2023-01-03 00:04:17 +0900

    01.02 프로그래머스Lv.2 JadenCase문자열 만들기(테스트 8번 실패 수정 본) => 마지막 공백을 따로 추가 함.

--- src/프로그래머스/JadenCase문자열만들기.java
+++ src/프로그래머스/JadenCase문자열만들기.java
@@ -4,17 +4,34 @@ public class JadenCase문자열만들기 {
 	public String solution(String s) {
 		String answer = "";
 		String[] strings = s.split(" ");
+		int num = 0;
+		for(int i = s.length() - 1; i >= 0; i--) {
+			if(s.charAt(i) == ' ') {
+				num ++;
+			}else {
+				break;
+			}
+		}
+		
 		for(int i = 0; i < strings.length; i++) {
+			String now = strings[i];
 			if(strings[i].length() == 0) {
 				answer += " ";
 			}else {
-				answer += strings[i].substring(0, 1).toUpperCase() + strings[i].substring(1).toLowerCase() + " "; 
-			}
-			if(answer.substring(answer.length() - 1, answer.length()).equals(" ")) {
-				return answer;
+				answer += now.substring(0, 1).toUpperCase() + now.substring(1).toLowerCase() + " "; 
 			}
 		}
-		return answer.substring(0, answer.length() - 1);
+		answer = answer.substring(0, answer.length() - 1);
+//		
+//		if(answer.substring(answer.length() - 1, answer.length()).equals(" ")) {
+//			System.out.println(answer.length());
+//			return answer.substring(0, answer.length() - 1);
+//		}
+		
+		for(int i =0; i < num; i++) {
+			answer += " ";
+		}
+		return answer;
 	}
 	public static void main(String[] args) {
 		JadenCase문자열만들기 g = new JadenCase문자열만들기();
